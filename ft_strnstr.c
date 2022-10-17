0b9cb12cd935497dd55a358e3b8010a275c4449b
pgorner <pgorner@student.42heilbronn.de>
2022-10-17 14:31:36 +0200

    Changed Names to appropriate

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -10,21 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-ft_strstr(const char *big, const char *little, size_t n);
+ft_strnstr(const char *big, const char *little, size_t n);
 {
-    int i;
+    size_t i;
+    size_t j;
 
     i = 0;
-
     if (*little == NULL)
-        return (*big);
-    
+        return ((char*)big);
 
-    while( *big[i] != '/0' && *little[i] != '/0' && i <= n)
-        if (*big[i] == *little [i])
-            while (*big[i] == *little [i])
-                i++;
-                if (little[i] == '/0')
-                    return 
-
+    while (big[i] != NULL && little[i] != NULL && i <= n) //while big isnt done and little isnt done and index is smaller than n
+    {
+        j = 0;
+        while ((big[i + j] == little [i]) && (little[j] != NULL) && (i + j < n)) //if big equals little at the point while it still has not surpassed the counter of i + j
+        {
+            j++;
+        }
+        if (j == ft_strlen(little))
+        (
+            return ((char*)big);
+        )
+        i++;
+    }
+    return (NULL);
 }
