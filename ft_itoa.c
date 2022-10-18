b41917def76329d5967f8207acb1e2fc4ccc19df
pgorner <pgorner@student.42heilbronn.de>
2022-10-18 19:12:48 +0200

    new files

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,29 +6,57 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 22:09:22 by pgorner           #+#    #+#             */
-/*   Updated: 2022/10/18 12:47:10 by pgorner          ###   ########.fr       */
+/*   Updated: 2022/10/18 18:32:05 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	return (0);
+}
 
 char	*ft_itoa(int n)
 {
-	int	counter;
-	int	sign;
 	char	*str;
 
-	counter = 0;
+	(str = (char *)malloc(sizeof(char) * 2));
+	if (!str)
+		return (NULL);
+	if (n == -2147483648)
+		return (ft_strcpy(str, "-2147483648"));
 	if (n < 0)
 	{
-		sign = 1;
-		n = n * -1;
+		str[0] = '-';
+		str[1] = '\0';
+		str = ft_strjoin(str, ft_itoa(-n));
 	}
-	while (n != 0)
+	else if (n >= 10)
+		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
+	else if (n < 10 && n >= 0)
 	{
-		n = n / 10;
-		counter++;
+		str[0] = n + '0';
+		str[1] = '\0';
 	}
-	str = (char *)malloc(sizeof(n) * (counter + 1 + sign));
-	return (0);
+	return (str);
 }
--- ft_striteri.c
+++ ft_striteri.c
@@ -1,39 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/17 20:03:40 by pgorner           #+#    #+#             */
-/*   Updated: 2022/10/18 18:16:48 by pgorner          ###   ########.fr       */
+/*   Created: 2022/10/18 18:36:36 by pgorner           #+#    #+#             */
+/*   Updated: 2022/10/18 18:42:02 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include "libft.h"
-
-char	*ft_strjoin(char const *s1, char const *s2)
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	char	*str;
-	long	i;
-	long	j;
+	int	i;
 
-	j = 0;
 	i = 0;
-	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (str == NULL)
-		return (NULL);
-	while (s1[i] != '\0')
+	if (s != 0 && f != 0)
 	{
-		str[i] = s1[i];
-		i++;
+		while (s[i] != '\0')
+		{
+			f(i, &s[i]);
+			i++;
+		}
 	}
-	while (s2[j] != '\0')
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
 }
