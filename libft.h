61be8612735dbcbd272ad77e6ea51a9d90f481c7
pgorner <pgorner@student.42heilbronn.de>
2022-12-19 14:07:37 +0100

    fixed shit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 23:47:31 by pgorner           #+#    #+#             */
-/*   Updated: 2022/12/18 17:09:51 by pgorner          ###   ########.fr       */
+/*   Updated: 2022/12/19 14:05:04 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # include <stddef.h>
 # include <string.h>
 # include <unistd.h>
-# include "./gnl/get_next_line.h"
+# include "gnl/get_next_line.h"
 
 // =============================================================================
 //								  ctype
--- makefile
+++ makefile
@@ -47,6 +47,7 @@ SRC = 	ft_memset.c \
 		ft_putendl_fd.c \
 		ft_putnbr_fd.c \
 		gnl/get_next_line.c \
+		gnl/get_next_line_utils.c \
 
 SRC_B =	ft_lstnew.c \
 		ft_lstadd_front.c \
