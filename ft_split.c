3785be882072a2834d9cac0b66ce487a5937fac8
pgorner <pgorner@student.42heilbronn.de>
2023-01-11 14:36:03 +0100

    change

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 16:09:58 by pgorner           #+#    #+#             */
-/*   Updated: 2023/01/11 14:35:05 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/01/11 14:35:54 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,7 @@ int	ft_countword(char const *s, char c)
 
 static char	**ft_free(char **tab)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (tab[i])
