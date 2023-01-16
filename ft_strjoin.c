21195f3ee60bb814332e1c2c5a81df297352b376
pgorner <pgorner@student.42heilbronn.de>
2023-01-16 13:46:02 +0100

    strjoin2

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: pgorner <pgorner@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 20:03:40 by pgorner           #+#    #+#             */
-/*   Updated: 2022/10/20 14:31:43 by pgorner          ###   ########.fr       */
+/*   Updated: 2023/01/16 13:45:48 by pgorner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,15 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	j;
 
 	j = 0;
-	i = 0;
-	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	i = -1;
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str == NULL)
 		return (NULL);
-	while (s1[i] != '\0')
-	{
-		str[i] = s1[i];
-		i++;
-	}
+	if (s1)
+		while (s1[++i] != '\0')
+			str[i] = s1[i];
 	while (s2[j] != '\0')
-	{
-		str[i + j] = s2[j];
-		j++;
-	}
-	str[i + j] = '\0';
-	return (str);
+		str[i++] = s2[j++];
+	str[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	return (free(s1), str);
 }
